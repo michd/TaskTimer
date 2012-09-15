845675604c2bd32763becba58c14834672d5f3c8
Michael Duerinckx <duerinckxmichael@gmail.com>
2012-09-15 15:20:14 +0100

    Added outline of the TaskTimer interface including documentation

--- assets/js/tasktimer.js
+++ assets/js/tasktimer.js
@@ -1,5 +1,96 @@
 (function($) {
 
+	var TaskTimer = function() {
+		
+		// Public interface of tasktimer object
+		return {
+
+			/**
+			 * Retrieves a task object by its HTML id attribute. 
+			 * 
+			 * Used to connect the HTML element that envelopes the task's interface
+			 * to the JS object. 
+			 * This uses a dictionary to map ids to task. The dictionary gets 
+			 * updated whenever a task is flushed and whenever a task is added.
+			 * 
+			 * @param string taskId identifier of the task to be looked up
+			 * @return Task|null The task object requested or null if not found
+			 */
+			"getTaskById": function(taskId) {
+
+			},
+
+			/**
+			 * Creates a new task object and returns it. 
+			 * 
+			 * The task object will be nameless and unactivated.
+			 * 
+			 * @return Task new empty task Object
+			 */
+			"createTask": function() {
+
+			},
+
+			/**
+			 * Sets the given task object as the active task.
+			 
+			 * This will add time to it every second.
+			 * 
+			 * @param  Task taskObject
+			 * @return null
+			 */
+			"activateTask": function(taskObject) {
+
+			},
+
+			/**
+			 * Creates a new TaskGroup. 
+			 * 
+			 * @return Taskgroup New nameless, empty TaskGroup object
+			 */
+			"createGroup": function() {
+
+			},
+
+			/**
+			 * Starts the timer, adding time to the activated task
+			 * 
+			 * @return null
+			 */
+			"startTimer": function() {
+
+			},
+
+			/**
+			 * Alias for startTimer
+			 * 
+			 * @return null
+			 */
+			"resumeTimer": function() {
+
+			},
+
+			/**
+			 * Pauses the timer, not adding any time to any tasks.
+			 * 
+			 * @return null
+			 */
+			"pauseTimer": function() {
+
+			}
+		};
+	};
+
+
+
+
+
+
+
+
+
+
+
 	var initalizeList = function() {
 		if ($('#tasklist ul').length == 0) {
 			$('#tasklist').append($('<ul>'));
